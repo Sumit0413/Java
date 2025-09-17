04e13dd8cb0815be6b0063f5ce714c8e4766a1f5
Sumit <sumit9229krsingh@gmail.com>
2025-09-18 00:45:58 +0530

    Pattern Printing

--- stars/Patterns.java
+++ stars/Patterns.java
@@ -372,6 +372,21 @@ for( int i = 1 ;i <= n ; i++ ){
 
 */
 
+int n =5 ;
+int counter = 1 ;
+
+for( int i = 1 ;i <= n ; i++ ){
+
+    for(int j = 1 ; j <=i ;j++ ){
+
+        System.out.print(counter + " ");
+        counter++;
+    }
+    System.out.println();
+   
+
+
+}
 
 
 //End    
