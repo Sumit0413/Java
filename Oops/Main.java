132f91ec09f9c619cf826d23536bd1ee50f6a746
Sumit <sumit9229krsingh@gmail.com>
2025-12-29 18:03:01 +0530

    Test commit

---
+++ Oops/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HI");
+
+        // Store 5 number
+        int[] numbers = new int[10];
+        numbers = new int[]{2,3,4,5,6,7,8,9,12,13};
+        System.out.println(numbers[6]);
+    }
+}
